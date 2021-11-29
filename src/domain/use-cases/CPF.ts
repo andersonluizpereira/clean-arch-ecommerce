2383054ce740e35a78a0540387c380987a9b0521
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2021-11-29 20:52:26 -0300

    adjust test

--- src/domain/use-cases/CPF.ts
+++ src/domain/use-cases/CPF.ts
@@ -1,30 +1,31 @@
-import { ValidarSeEstaVazioeNulo, ValidarTamanhoDaString, 
-         RemoverCaracterEspecial, ValidarFormatoCpf, 
-         ValidarLogicaDeDigitosCpf, ValidarPrimeiroDigitoContador, 
-         ValidarSegundoDigitoContador, CalcularRestoDivisaoSaoCpf } from '@/domain/use-cases'
+import {
+  ValidarSeEstaVazioeNulo, ValidarTamanhoDaString,
+  RemoverCaracterEspecial, ValidarFormatoCpf,
+  ValidarLogicaDeDigitosCpf, ValidarPrimeiroDigitoContador,
+  ValidarSegundoDigitoContador, CalcularRestoDivisaoSaoCpf
+} from '@/domain/use-cases'
 
 export default class CPF {
   validarCpf(cpf: string): boolean {
     if (!ValidarSeEstaVazioeNulo(cpf)) return false
     if (!ValidarTamanhoDaString(cpf)) return false
     cpf = RemoverCaracterEspecial(cpf);
-
     if (!ValidarFormatoCpf(cpf)) return false
-        let NUMERO_CALCULO_CPF = 11;
-        let primeirosDigitosDoCpf = 0, segundoDigitosDoCpf = 0;
-        let numeroDoDigitoResultoDoCpf;
-        let { primeiroDigitoCpf, segundoDigitoCpf, restoDiviSaoCpf } = ValidarLogicaDeDigitosCpf(cpf);
-        restoDiviSaoCpf = CalcularRestoDivisaoSaoCpf(primeiroDigitoCpf,NUMERO_CALCULO_CPF);
-        primeirosDigitosDoCpf = ValidarPrimeiroDigitoContador({
-          restoDiviSaoCpf,
-          primeirosDigitosDoCpf,
-          NUMERO_CALCULO_CPF
-        });
-        segundoDigitoCpf = ValidarSegundoDigitoContador(primeirosDigitosDoCpf, segundoDigitoCpf)
-        restoDiviSaoCpf = CalcularRestoDivisaoSaoCpf(segundoDigitoCpf, NUMERO_CALCULO_CPF);
-        restoDiviSaoCpf < 2 ? segundoDigitosDoCpf = 0 : segundoDigitosDoCpf = NUMERO_CALCULO_CPF - restoDiviSaoCpf;
-        let numeroDigitoVerificador = cpf.substring(cpf.length - 2, cpf.length);
-        numeroDoDigitoResultoDoCpf = "" + primeirosDigitosDoCpf + "" + segundoDigitosDoCpf;
-        return numeroDigitoVerificador == numeroDoDigitoResultoDoCpf;
+    let NUMERO_CALCULO_CPF = 11;
+    let primeirosDigitosDoCpf, segundoDigitosDoCpf = 0;
+    let numeroDoDigitoResultoDoCpf;
+    let { primeiroDigitoCpf, segundoDigitoCpf, restoDiviSaoCpf } = ValidarLogicaDeDigitosCpf(cpf);
+    restoDiviSaoCpf = CalcularRestoDivisaoSaoCpf(primeiroDigitoCpf, NUMERO_CALCULO_CPF);
+    primeirosDigitosDoCpf = ValidarPrimeiroDigitoContador({
+      restoDiviSaoCpf,
+      primeirosDigitosDoCpf,
+      NUMERO_CALCULO_CPF
+    });
+    segundoDigitoCpf = ValidarSegundoDigitoContador(primeirosDigitosDoCpf, segundoDigitoCpf)
+    restoDiviSaoCpf = CalcularRestoDivisaoSaoCpf(segundoDigitoCpf, NUMERO_CALCULO_CPF);
+    restoDiviSaoCpf < 2 ? segundoDigitosDoCpf = 0 : segundoDigitosDoCpf = NUMERO_CALCULO_CPF - restoDiviSaoCpf;
+    let numeroDigitoVerificador = cpf.substring(cpf.length - 2, cpf.length);
+    numeroDoDigitoResultoDoCpf = "" + primeirosDigitosDoCpf + "" + segundoDigitosDoCpf;
+    return numeroDigitoVerificador == numeroDoDigitoResultoDoCpf;
   }
 }

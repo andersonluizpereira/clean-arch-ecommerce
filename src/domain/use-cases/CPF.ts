4e8e2348ce519cc3297d70bdf32034d8fe5857c2
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2021-11-29 20:49:49 -0300

    adjust test

--- src/domain/use-cases/CPF.ts
+++ src/domain/use-cases/CPF.ts
@@ -14,9 +14,7 @@ export default class CPF {
         let primeirosDigitosDoCpf = 0, segundoDigitosDoCpf = 0;
         let numeroDoDigitoResultoDoCpf;
         let { primeiroDigitoCpf, segundoDigitoCpf, restoDiviSaoCpf } = ValidarLogicaDeDigitosCpf(cpf);
-
         restoDiviSaoCpf = CalcularRestoDivisaoSaoCpf(primeiroDigitoCpf,NUMERO_CALCULO_CPF);
-       
         primeirosDigitosDoCpf = ValidarPrimeiroDigitoContador({
           restoDiviSaoCpf,
           primeirosDigitosDoCpf,
@@ -24,9 +22,7 @@ export default class CPF {
         });
         segundoDigitoCpf = ValidarSegundoDigitoContador(primeirosDigitosDoCpf, segundoDigitoCpf)
         restoDiviSaoCpf = CalcularRestoDivisaoSaoCpf(segundoDigitoCpf, NUMERO_CALCULO_CPF);
-        if (restoDiviSaoCpf < 2) segundoDigitosDoCpf = 0;
-        else segundoDigitosDoCpf = NUMERO_CALCULO_CPF - restoDiviSaoCpf;
-
+        restoDiviSaoCpf < 2 ? segundoDigitosDoCpf = 0 : segundoDigitosDoCpf = NUMERO_CALCULO_CPF - restoDiviSaoCpf;
         let numeroDigitoVerificador = cpf.substring(cpf.length - 2, cpf.length);
         numeroDoDigitoResultoDoCpf = "" + primeirosDigitosDoCpf + "" + segundoDigitosDoCpf;
         return numeroDigitoVerificador == numeroDoDigitoResultoDoCpf;
